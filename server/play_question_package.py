2f73e4f2d85195da5916058897d7291b84c7bbd2
FilipRich <filip.richeberg@hotmail.com>
2021-05-04 17:16:11 +0200

    Many changes
    Co-Authored-By: antonsandman <75663580+antonsandman@users.noreply.github.com>

--- server/play_question_package.py
+++ server/play_question_package.py
@@ -1,7 +1,9 @@
-from db_zingo import read_from_db, update_db, execute_procedure
+from db_zingo import read_from_db, update_db, execute_procedure, cursor
 from random import shuffle
+from flask import Flask, redirect, url_for, render_template, request, session
 import pyodbc
 import time
+
 
 '''
 chosen_qp = "Blandade sportfrågor"
@@ -61,23 +63,62 @@ def save_result():
     # spara slutresultat i databas
 '''
 
-def save_qp_to_db1():
-    qp_name = request.form['qp_name']
-    qp_desc = request.form['qp_desc']
+def apply_tags_to_qp(tag_list):
+    cursor.execute(f"select qp_id from question_package where qp_name = '{session['qp_name']}'")
+    res = cursor.fetchone()
+    qp_id = res[0]
+    print(qp_id)
+    print(tag_list)
+    for i in tag_list:
+        print(i)
+        cursor.execute(f"select tag_id from tag where tag_description = '{i}'")
+        row = cursor.fetchone()
+        tag_id = row[0]
+        cursor.execute(f"insert into question_package_tag (qp_id, tag_id) values ({qp_id}, {tag_id})")
+        cursor.commit()
+
+def save_qp_to_db1(qp_name, qp_desc, qp_tags):
+    cursor.execute(f"select [user_id] from [user] where e_mail = '{session['email']}'")
+    res = cursor.fetchone()
+    user_id = res[0]
+    
+
+    
     #!add session id for user to relate 'created_by' in db, also add it into insert!
 
-    cursor.execute(f"insert into Zingo_DB.dbo.question_package (qp_description, qp_name) values ('{qp_desc}', '{qp_name}')")
-    conn.commit()
+    qp_ndu = []
+    nl = []
+    qp_ndu.append(qp_name.split(" "))
+    qp_ndu.append(qp_desc.split(" "))
+    for i in qp_ndu:
+        for x in i:
+            y = filter(str.isalnum, x)
+            t = "".join(y)
+            nl.append(t)
 
-    return qp_name
+    if check_words_aginst_db(nl):
+        word = check_words_aginst_db(nl)
+        print(word)
+        print('true')
 
-def get_question():
+    else:
+        print('false')
+        session['qp_name'] = qp_name
+        print(f"{session['email']}")
+        print(qp_name)
+        print(qp_desc)
+        print(qp_tags)
+        print(user_id)
+        cursor.execute(f"insert into question_package (qp_description, created_by, qp_name) values ('{qp_desc}', {user_id}, '{qp_name}')")
+        cursor.commit()
+        apply_tags_to_qp(qp_tags)
+        
+
+    
+
+def get_question(question, answer_1, answer_2, answer_3, answer_4):
     # get form from client
-    question = request.form['question']
-    answer_1 = request.form['answer_1']
-    answer_2 = request.form['answer_2']
-    answer_3 = request.form['answer_3']
-    answer_4 = request.form['answer_4']
+    
     # split words into list and remove non-alphnum chars
     ql = []
     nt = []
@@ -93,14 +134,17 @@ def get_question():
             nt.append(t)
     # check if words exist in db table for profanity words
     if check_words_aginst_db(nt):
-        #if control is not ok(true), DON'T clear form POP-UP/somthing profanity-word.
         word = check_words_aginst_db(nt)
-        print(word)
-        print('true')
+        print(word) #wip
+        print('true') #wip
     else:
-        #if control is ok, save q+ans in qp_list in db
-        #clear q+a form for user
-        print('false')
+        #-- before running: !Control that a session with qp_name is created during creation of new qp!
+        qp_name = session['qp_name']
+
+        current_qp_id = cursor.execute(f"select qp_id from question_package where qp_name = '{qp_name}'")
+        cursor.execute(f"insert into Zingo_DB.dbo.question (question, answer_1_correct, answer_2, answer_3, answer_4, qp_id) values ('{question}', '{answer_1}', '{answer_2}', '{answer_3}', '{answer_4}', '{current_qp_id}')")
+        cursor.commit()
+        print('false') #wip
 
 def check_words_aginst_db(all_words):
     # all_words = list
