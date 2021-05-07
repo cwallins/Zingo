37556d56ebafc9d8eea8d3a391be37dd8e83fd82
wongren <81237890+wongren@users.noreply.github.com>
2021-05-07 12:08:07 +0200

    finslipat html,css
    Co-Authored-By: nataliegull <81237695+nataliegull@users.noreply.github.com>
    Co-Authored-By: cwallins <80338047+cwallins@users.noreply.github.com>

--- server/static/script.js
+++ server/static/script.js
@@ -1,14 +1,11 @@
-function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("Invite_player_url");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    alert("Copied URL: " + copyText.value); 
-  }
+
+/*Popover for 'Rules'*/
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+  trigger: 'focus'
+})
+
