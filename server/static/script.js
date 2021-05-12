183d1a9aae1ed1da750911dc8c6ac2553cb8e80a
nataliegull <nataliegullbing@Natalies-MacBook-Air.local>
2021-05-12 17:56:31 +0200

    JS-kod för menyn
    Skrivit js så att element i menyn visas/göms beroende på om man är inloggad eller inte. Plus fixat så att "Rules"-knappen fungerar ordentligt, och att term & conditions sökvägen fungerar.

--- server/static/script.js
+++ server/static/script.js
@@ -1,24 +1,50 @@
 
-/*Popover for 'Rules' which only works when in base.html for some reason 
+/*Popover for 'Rules' --> Works when executed in base.html */
 
-<script>
-    $( function( )
-    {
-      $( "[data-toggle= 'popover'" ).popover( );
-    } );
+var show_rules
+  /*Popover for 'Rules' --> Works when executed in base.html */
+  $(document).ready(function(){
+      $( "[data-toggle='popover'" ).popover( );
+    });
 
-    $( ".popover-dismiss" ).popover( 
+    $( ".popover_dismiss" ).popover( 
     {
         trigger: "focus"
-    } );
-
-</script>
-*/
+    });
 
 
-/*Time-progress
+/*Prov-kod till show_and_hide --> First variable is proven to work when executed in base.html but crash immediately after. The rest in untested.*/
 
-/*This part is maybe superfluous
+var hide_when_guest
+$(document).ready(function() {
+  $('#logout, .nav-link-signed-in').hide();
+});
+
+var show_hide_when_signed_in
+$(document).ready(function() {
+  if "'loggedin' in session:" /* quote-python-kod (checks if the user is a registered user)--> How rewrite this to work here? */
+    $('#sign_in_button').on('click', function() {
+      $('#logout, .nav-link-signed-in').show();
+      $('.nav-link-guest').hide();
+
+    })
+  else
+    $('#logout, .nav-link-signed-in').hide(); /* Lazy code, may not work. */
+});
+
+var show_hide_When_reg_user_turned_to_guest
+$(document).ready(function() {
+  $('#logout').on('click', function() {
+    $('#logout, .nav-link-signed-in, nav-link-guest ').toggle();
+  });
+});
+
+/* End of code for show_hide */
+
+
+/*Time-progress --> Not tested!!
+
+This part may be superfluous
 var bar = document.getElementById('progress'),
     time = 0, max = 5,
     int = setInterval(function() {
