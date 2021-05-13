1bf215100129cea1499dbcc8e0b7f9c7ba21659f
FilipRich <filip.richeberg@hotmail.com>
2021-05-13 09:54:27 +0200

    js test
    Co-Authored-By: nataliegull <81237695+nataliegull@users.noreply.github.com>
    Co-Authored-By: cwallins <80338047+cwallins@users.noreply.github.com>
    Co-Authored-By: wongren <81237890+wongren@users.noreply.github.com>

--- server/static/script.js
+++ server/static/script.js
@@ -1,24 +1,52 @@
 
-/*Popover for 'Rules' which only works when in base.html for some reason 
 
-<script>
-    $( function( )
-    {
-      $( "[data-toggle= 'popover'" ).popover( );
-    } );
-
-    $( ".popover-dismiss" ).popover( 
-    {
-        trigger: "focus"
-    } );
-
-</script>
-*/
+/*Popover for 'Rules' --> Works when executed in base.html */
 
 
-/*Time-progress
+  /*Popover for 'Rules' --> Works when executed in base.html */
+var show_rules
+$(document).ready(function(){
+  $( "[data-toggle='popover'" ).popover( );
+});
 
-/*This part is maybe superfluous
+$( "#popover_dismiss" ).popover( 
+{
+    trigger: "focus"
+});
+
+
+/*Prov-kod till show_and_hide --> First variable is proven to work when executed in base.html but crash immediately after. The rest in untested.*/
+
+var hide_when_guest
+$(document).ready(function() {
+  $('#logout, .nav-link-signed-in').hide();
+});
+
+var show_hide_when_signed_in
+$(document).ready(function() {
+  if 'loggedin' in session: /* quote-python-kod (checks if the user is a registered user)--> How rewrite this to work here? */
+    $('#sign_in_button').on('click', function() {
+      $('#logout, .nav-link-signed-in').show();
+      $('.nav-link-guest').hide();
+
+    })
+  else
+    $('#logout, .nav-link-signed-in').hide(); /* Lazy code, may not work. */
+});
+
+var show_hide_When_reg_user_turned_to_guest
+$(document).ready(function() {
+  $('#logout').on('click', function() {
+    $('#logout, .nav-link-signed-in, nav-link-guest ').toggle();
+  });
+});
+
+/* End of code for show_hide */
+
+
+/*Time-progress --> Not tested!!
+
+This part may be superfluous
 var bar = document.getElementById('progress'),
     time = 0, max = 5,
     int = setInterval(function() {
