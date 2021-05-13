d8adbb0fdf578ed7ca06aa8aa431ec473a61e68f
wongren <81237890+wongren@users.noreply.github.com>
2021-05-13 12:02:05 +0200

    css fix
    Co-Authored-By: nataliegull <81237695+nataliegull@users.noreply.github.com>
    Co-Authored-By: cwallins <80338047+cwallins@users.noreply.github.com>

--- server/static/script.js
+++ server/static/script.js
@@ -4,44 +4,16 @@
 
 
   /*Popover for 'Rules' --> Works when executed in base.html */
-var show_rules
-$(document).ready(function(){
-  $( "[data-toggle='popover'" ).popover( );
-});
-
-$( "#popover_dismiss" ).popover( 
-{
-    trigger: "focus"
-});
-
-
-/*Prov-kod till show_and_hide --> First variable is proven to work when executed in base.html but crash immediately after. The rest in untested.*/
-
-var hide_when_guest
-$(document).ready(function() {
-  $('#logout, .nav-link-signed-in').hide();
-});
-
-var show_hide_when_signed_in
-$(document).ready(function() {
-  if 'loggedin' in session: /* quote-python-kod (checks if the user is a registered user)--> How rewrite this to work here? */
-    $('#sign_in_button').on('click', function() {
-      $('#logout, .nav-link-signed-in').show();
-      $('.nav-link-guest').hide();
-
-    })
-  else
-    $('#logout, .nav-link-signed-in').hide(); /* Lazy code, may not work. */
-});
-
-var show_hide_When_reg_user_turned_to_guest
-$(document).ready(function() {
-  $('#logout').on('click', function() {
-    $('#logout, .nav-link-signed-in, nav-link-guest ').toggle();
+  $(document).ready(function(){
+    $( "[data-toggle='popover'" ).popover( );
   });
-});
 
-/* End of code for show_hide */
+  $( ".popover_dismiss" ).popover(
+      {
+        trigger: "focus"
+      } 
+    ); 
+
 
 
 /*Time-progress --> Not tested!!
@@ -77,5 +49,3 @@ countdown(function() {
 END OF REAL CODE
 
 */
-
-
